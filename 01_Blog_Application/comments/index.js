0d7783892d5005577a161fb3518b8f35cf2f27cd
Anmol Verma <anmolverma102002@gmail.com>
2022-10-21 21:48:08 +0530

    Express work done, to react work:rocket:

---
+++ 01_Blog_Application/comments/index.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const PORT = 4001;
+
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/", (req, res) => {
+  res.send("This is comments server");
+});
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running live on: http://localhost:${PORT}`);
+});
