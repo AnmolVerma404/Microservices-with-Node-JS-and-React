7793900a5d7bc140d643e7310214982e0b1b6eeb
Anmol Verma <anmolverma102002@gmail.com>
2022-11-04 16:57:05 +0530

    Error Handling:rocket:

--- 04_Ticketing/auth/src/index.ts
+++ 04_Ticketing/auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const PORT = 3000;
 const app = express();
@@ -14,6 +15,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App is listining!!! on http://localhost:${PORT}`);
--- 04_Ticketing/auth/src/routes/signup.ts
+++ 04_Ticketing/auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
-router.get("/api/users/signup", (req, res) => {
-  res.send("Hi There!");
-});
+router.get(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 character"!),
+  ],
+  (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      throw new RequestValidationError(error.array());
+    }
+    const { user, password } = req.body;
+    throw new DatabaseConnectionError();
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
