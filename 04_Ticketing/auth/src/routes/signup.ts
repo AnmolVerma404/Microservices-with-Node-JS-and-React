b3adfdcc8e7bc8219b3f41bf40ff935280cf48d3
Anmol Verma <anmolverma102002@gmail.com>
2022-11-07 20:49:46 +0530

    signin completed:rocket:

--- 04_Ticketing/auth/src/routes/signup.ts
+++ 04_Ticketing/auth/src/routes/signup.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { body } from "express-validator";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 import jwt from "jsonwebtoken";
-
+import { validateRequest } from "../middlewares/validate-request";
 const router = express.Router();
 
 router.get(
@@ -17,11 +15,8 @@ router.get(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 character"!),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      throw new RequestValidationError(error.array());
-    }
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
