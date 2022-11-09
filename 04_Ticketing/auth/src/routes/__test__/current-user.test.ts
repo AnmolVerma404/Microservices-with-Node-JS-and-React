5eae0daee2bdaf3d395db85ee3c612c00979fb4c
Anmol Verma <anmolverma102002@gmail.com>
2022-11-09 17:02:21 +0530

    current-user test:rocket:

--- 04_Ticketing/auth/src/routes/__test__/current-user.test.ts
+++ 04_Ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -2,20 +2,24 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("respond with detail about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
     .send()
-    .expect(201);
+    .expect(200);
 
   expect(response.body.currentuser.email).toEqual("test@test.com");
+});
+
+it("respond with null of not authenticated", async () => {
+  const cookie = await global.signin();
+
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentuser).toEqual(null);
 });
